8fce7668c27e3546da94f81796194a36afd1db54
gonzalocase <gonzalo@caseonit.com>
2016-09-27 16:29:50 +0200

    first_commit

---
+++ app/src/main/java/com/caseonit/library/Asd.java
@@ -0,0 +1,14 @@
+package com.caseonit.library;
+
+/**
+ * @author Gonzalo on 27/09/2016.
+ */
+public class Asd {
+
+    private String asd;
+
+    public void setAsd(String asd) {
+        this.asd = asd;
+    }
+
+}
